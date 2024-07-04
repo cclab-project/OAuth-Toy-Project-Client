fdeef7a3679aa01cc0825a668ce453029d210867
SiHoon61 <dlatlgns000@gmail.com>
2024-07-04 21:19:04 +0900

    Add: get db blob

--- src/pages/StoreAdminJoin/StoreAdminJoin.js
+++ src/pages/StoreAdminJoin/StoreAdminJoin.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 import {
     Container,
     
  } from './style';
 const StoreAdminJoin = () => {
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/localhost:8080/open-api/store/temp`, {
+                    
+                });
+                console.log(response.data);
+            } catch (error) {
+                console.log('에러');
+            }
+        };
+        getData();
+    }, []);
     return (
         <>
             <Container>
-                
+               
             </Container>
         </>
     );
