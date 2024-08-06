cd16f014db17f38ec95cf8b38124029284b9c341
SiHoon61 <dlatlgns000@gmail.com>
2024-08-06 17:38:50 +0900

    Add: noSubmitHandler

--- src/pages/StoreAdminLogin/StoreAdminLogin.js
+++ src/pages/StoreAdminLogin/StoreAdminLogin.js
@@ -36,6 +36,24 @@ const StoreAdminLogin = () => {
             alert('로그인 실패');
         }
     }
+    const noSubmitHandler = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL2}/login`, qs.stringify({
+                username: email,
+                password: password,
+            }), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                withCredentials: true
+            });
+            console.log(response);
+        } catch (error) {
+            console.error('Error logging in:', error);
+            alert('로그인 실패');
+        }
+    }
     const goAdminJoin = () => {
         navigate("/AdminJoin");
     }
@@ -65,6 +83,31 @@ const StoreAdminLogin = () => {
                     </SubmitButton>
                 </InputForm>
                 <Join onClick={goAdminJoin}>
+                    관리자 회원가입
+                </Join>
+            </Container>
+            <Container>
+                <h2>리디이랙트X 로그인</h2>
+                <InputForm onSubmit={loginSubmitHandler}>
+                    <InputBox>
+                        <InputText
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='이메일' />
+                    </InputBox>
+                    <InputBox>
+                        <InputText
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder='비밀번호' />
+                    </InputBox>
+                    <SubmitButton type='submit'>
+                        로그인
+                    </SubmitButton>
+                </InputForm>
+                <Join onClick={noSubmitHandler}>
                     관리자 회원가입
                 </Join>
             </Container>
